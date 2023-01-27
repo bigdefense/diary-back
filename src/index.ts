4aebcc7091cb6e21f9e72b55c00b999832a12a6d
KUD <kimud6003@gmail.com>
2023-01-27 17:59:28 +0900

    -Feature index.ts
    - routes를 배열로 만들어 App의 생성자에게 전달

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import App from './app';
+import Hello from './routes/hello.route';
+import World from './routes/world.route';
 
-const app = new App();
+const app = new App([new Hello(), new World()]);
 
 app.listen();
