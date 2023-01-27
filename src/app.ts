d7108d61cd5e99ea1e964c466365f41cc3535fd6
KUD <kimud6003@gmail.com>
2023-01-27 17:52:56 +0900

    -Feature App.ts
    - MiddleWare 작성 (cors,hpp,helmet,cookieparser)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import hpp from 'hpp';
 
 class App{
   public app: express.Application;
   public port: number;
+    this.middlewares();
+    this.middlewares();
 
   constructor(){
     this.app = express();
@@ -17,6 +23,14 @@ class App{
       console.log(`Server is listening on port ${this.port}`);
     });
   }
+  private middlewares(){
+    this.app.use(helmet({}))
+    this.app.use(hpp({}))
+    this.app.use(cors({}))
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+  } 
 }
 
 export default App;
