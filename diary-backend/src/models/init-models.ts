2abb434b2c1021e755eacdbad64de4b940a8ec07
KUD <kimud6003@gmail.com>
2023-01-30 19:54:50 +0900

    - Feature 테이블 관계성 정리
    - pages와 posts,stickers,users 관계 정립

---
+++ diary-backend/src/models/init-models.ts
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+import postsInitial from './posts';
+import pagesInitial from './pages';
+import stickersInitial from './stickers';
+import usersInitial from './users';
+import sequelize from '../database';
+
+const Users = usersInitial(sequelize);
+const Posts = postsInitial(sequelize);
+const Pages = pagesInitial(sequelize);
+const Stickers = stickersInitial(sequelize);
+
+Pages.hasMany(Posts, {as: 'posts', foreignKey: 'page_id'});
+Pages.hasMany(Stickers, {as: 'stickers', foreignKey: 'page_id'});
+Pages.belongsTo(Users, {as: 'users', foreignKey: 'user_id'});
+Posts.belongsTo(Pages, {as: 'pages', foreignKey: 'page_id'});
+Stickers.belongsTo(Pages, {as: 'pages', foreignKey: 'page_id'});
+Users.hasMany(Pages, {as: 'pages', foreignKey: 'user_id'});
+
+const initModels = {
+  Users,
+  Posts,
+  Pages,
+  Stickers,
+  sequelize,
+  Sequelize,
+};
+
+export default initModels;
