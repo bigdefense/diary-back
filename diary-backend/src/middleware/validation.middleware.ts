0c214c47dfc27ae946f2cc5a4482c1978e451479
kimud6003 <kimud6003@gmail.com>
2023-02-01 10:39:16 +0000

    - Feature Validation 미들웨어 설정
    - RequestHandler를 통해 데이터 형식 확인 절차

---
+++ diary-backend/src/middleware/validation.middleware.ts
@@ -0,0 +1,33 @@
+import exceptError from '@/utils/excetpError';
+import {plainToInstance} from 'class-transformer';
+import {validate, ValidationError} from 'class-validator';
+import {RequestHandler, Response, NextFunction} from 'express';
+
+const validationMiddleware = (
+  type: any,
+  value: string | 'body' | 'query' | 'params' = 'body',
+  skipMissingProperties = false,
+  whitelist = true,
+  forbidNonWhitelisted = true,
+): RequestHandler => {
+  return (req: any, res: Response, next: NextFunction) => {
+    validate(plainToInstance(type, req[value]), {
+      skipMissingProperties,
+      whitelist,
+      forbidNonWhitelisted,
+    }).then((errors: ValidationError[]) => {
+      if (errors.length > 0) {
+        const message = errors
+          .map((error: ValidationError) =>
+            Object.values(error.constraints ? error.constraints : ''),
+          )
+          .join(', ');
+        next(new exceptError(400, message));
+      } else {
+        next();
+      }
+    });
+  };
+};
+
+export default validationMiddleware;
