9a2e36f6b917dc0d3f798a8cd95ed912d80a998c
kimud6003 <kimud6003@gmail.com>
2023-02-01 10:42:39 +0000

    - Feature UserController 생성
    - rest를 통해 받은 내용물을 service를 호출하여 실행

---
+++ diary-backend/src/controller/users.controller.ts
@@ -0,0 +1,73 @@
+import {Request, Response, NextFunction} from 'express';
+
+import {Users} from '@/interface/users.interface';
+import {UserService} from '@/service/users.service';
+
+class UsersController {
+  public userService = new UserService();
+  // 회원가입
+  public userSignUp = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const user: Users = req.body;
+      const result = await this.userService.userSignUp(user);
+      res.status(201).json({data: result, message: 'signup'});
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // 로그인
+  public userSignIn = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const {email, password} = req.body;
+      const {cookie, findUser}: {cookie: string; findUser: Users} =
+        await this.userService.userSignIn(email, password);
+      res.setHeader('Set-Cookie', [cookie]);
+      res.status(200).json({data: findUser, message: 'login'});
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public userSignOut = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const {email, password} = req.body;
+      const {cookie, findUser} = await this.userService.userSignIn(
+        email,
+        password,
+      );
+      res.setHeader('Set-Cookie', [cookie]);
+      res.status(200).json({data: findUser, message: 'login'});
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // 이메일 중복체중
+  public userEmailDup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const {email} = req.body;
+      const result = await this.userService.userEmailDuplicate(email);
+      await res.send(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+export default UsersController;
