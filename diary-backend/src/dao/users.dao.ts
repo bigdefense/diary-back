62df0874303b201d171581a6ee1bf1b7b8b5a02a
kimud6003 <kimud6003@gmail.com>
2023-02-01 10:38:02 +0000

    - Feature UserDao 생성
    - findUserEmail
    - createUser

--- diary-backend/src/dao/users.dao.ts
+++ diary-backend/src/dao/users.dao.ts
@@ -1,35 +1,40 @@
 import {hash} from 'bcrypt';
-import {createUserDto} from '@/dto/users.dto';
+import {UserDto} from '@/dto/users.dto';
 import {Users} from '@/interface/users.interface';
 import models from '@/models/init-models';
 import {isEmpty} from 'class-validator';
+import {logger} from '@/utils/logger';
+import sequelize from '@/database';
 import {Transaction} from 'sequelize';
+import exceptError from '@/utils/excetpError';
 
 class UsersDao {
   public users = models.Users;
 
-  public async findUserById(userId: string): Promise<Users> {
-    if (isEmpty(userId)) throw new Error(`${400}You are not userId`);
-    const findUser: Users | null = await this.users.findByPk(userId);
-    if (!findUser) throw new Error(`${400}You are not user`);
+  public async findUserByEmail(email: string): Promise<Users> {
+    if (isEmpty(email)) throw new exceptError(400, `You didn't give userEmail`);
+    const findUser: Users | null = await this.users.findOne({where: {email}});
+    if (!findUser) throw new exceptError(400, `You are not user`);
     return findUser;
   }
 
-  public async createUser(userData: createUserDto): Promise<Users> {
-    if (isEmpty(userData)) throw new Error(`${400}You're not userData`);
-
-    const findUser: Users | null = await this.users.findOne({
-      where: {email: userData.email},
-    });
-    if (findUser)
-      throw new Error(`${409}, you're email ${userData.email} already exists`);
-
-    const hashedPassword = await hash(userData.password, 10);
-    const createUserData: Users = await this.users.create({
-      ...userData,
-      password: hashedPassword,
-    });
-    return createUserData;
+  public async createUser(userData: UserDto): Promise<Users | void> {
+    const transaction: Transaction = await sequelize.transaction();
+    try {
+      const hashedPassword = await hash(userData.password, 10);
+      const createUserData: Users = await this.users.create(
+        {
+          ...userData,
+          password: hashedPassword,
+        },
+        {transaction: transaction},
+      );
+      await transaction.commit();
+      return createUserData;
+    } catch (e) {
+      logger.error(e);
+      await transaction.rollback();
+    }
   }
 }
 
