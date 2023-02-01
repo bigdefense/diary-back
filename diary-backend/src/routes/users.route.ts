d0e802c827b4e2b6025487cbbd11e48c7bd07f7a
kimud6003 <kimud6003@gmail.com>
2023-02-01 10:41:29 +0000

    - Feature Users.route
    - middle웨어 추가

---
+++ diary-backend/src/routes/users.route.ts
@@ -0,0 +1,30 @@
+import {Router} from 'express';
+import UsersController from '@/controller/users.controller';
+import validationMiddleware from '@/middleware/validation.middleware';
+import {UserDto} from '@/dto/users.dto';
+import authMiddleware from '@/middleware/auth.middleware';
+
+class Users {
+  public path = '/users';
+  public router = Router();
+  public usersController = new UsersController();
+  constructor() {
+    this.router.post(
+      `${this.path}/signup`,
+      validationMiddleware(UserDto, 'body'),
+      this.usersController.userSignUp,
+    );
+    this.router.post(
+      `${this.path}/signin`,
+      validationMiddleware(UserDto, 'body'),
+      this.usersController.userSignIn,
+    );
+    this.router.post(
+      `${this.path}/emaildup`,
+      authMiddleware,
+      this.usersController.userEmailDup,
+    );
+  }
+}
+
+export default Users;
