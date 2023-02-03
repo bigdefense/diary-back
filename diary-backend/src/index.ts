621bf53e47d3876cd4ad7c9e8e0aefb63c2edb1a
kimud6003 <kimud6003@gmail.com>
2023-02-03 13:15:15 +0000

    - Feature User Rout 추가

--- diary-backend/src/index.ts
+++ diary-backend/src/index.ts
@@ -1,7 +1,8 @@
 import App from './app';
 import Hello from './routes/hello.route';
+import Users from './routes/users.route';
 import World from './routes/world.route';
 
-const app = new App([new Hello(), new World()]);
+const app = new App([new Hello(), new World(), new Users()]);
 
 app.listen();
