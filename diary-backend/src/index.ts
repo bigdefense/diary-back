5aeb97f26dfab82a536682c38b721a84501581c0
kimud6003 <kimud6003@gmail.com>
2023-04-06 10:18:16 +0000

    Feature Daily 라우터 추가
    - Daily 라우터 추가

--- diary-backend/src/index.ts
+++ diary-backend/src/index.ts
@@ -2,7 +2,8 @@ import App from './app';
 import Hello from './routes/hello.route';
 import Users from './routes/users.route';
 import World from './routes/world.route';
+import Daily from './routes/daily.route';
 
-const app = new App([new Hello(), new World(), new Users()]);
+const app = new App([new Daily(), new Hello(), new World(), new Users()]);
 
 app.listen();
