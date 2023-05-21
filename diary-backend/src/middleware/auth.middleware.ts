e62642acfb8593af7caba0da9d4f7665f57fcd7f
kimud6003 <kimud6003@gmail.com>
2023-05-21 13:24:21 +0000

    - token 리펙토링

--- diary-backend/src/middleware/auth.middleware.ts
+++ diary-backend/src/middleware/auth.middleware.ts
@@ -1,28 +1,8 @@
 import {NextFunction, Response} from 'express';
-import {verify, sign, decode} from 'jsonwebtoken';
 import {JwtToken, RequestWithUser} from '../interface/auth.interface';
-import models from '../models/init-models';
 import exceptError from '../utils/excetpError';
-import f from 'session-file-store';
-
-const secretKey = 'secret';
-
-const getCookie = (req: RequestWithUser, str: string) => {
-  const coockie = req.cookies[str];
-  if (coockie) return coockie;
-
-  const header = req.header(str);
-  if (header) return header.split('Bearer')[1];
-
-  return null;
-};
-
-const tokenVerify = (token: string, next: NextFunction): JwtToken | void => {
-  return verify(token, secretKey, (err, data) => {
-    if (!err) return false;
-    if (err.name === 'TokenExpiredError') return true;
-  });
-};
+import {getCookie} from '@/utils/getCookie';
+import {tokenVerify} from '@/utils/tokenVerify';
 
 export const authMiddleware = async (
   req: RequestWithUser,
@@ -31,46 +11,16 @@ export const authMiddleware = async (
 ) => {
   try {
     const Authorization = getCookie(req, 'Authorization');
-    const Refresh = getCookie(req, 'Refresh');
-    if (!Authorization)
-      return next(new exceptError(404, 'AccessToken이 없습니다!'));
+    if (!Authorization) throw new exceptError(401, `토큰 정보가 없습니다`);
 
-    const isAccessExpired = tokenVerify(Authorization, next);
-    const isRefreshExpired = tokenVerify(Refresh, next);
-    const accessData = decode(Authorization) as JwtToken;
-    const {id} = accessData.data;
-    const findUser = await models.Users.findOne({where: {id}});
-    if (!findUser)
-      return next(new exceptError(401, '유효하지 않은 사용자입니다'));
+    const isAccessExpired = tokenVerify(Authorization);
 
-    if (isAccessExpired && isRefreshExpired)
-      return next(new exceptError(401, 'API 사용권한이 없습니다!'));
-
-    if (isAccessExpired && !isRefreshExpired) {
-      if (findUser.refresh !== Refresh)
-        return next(new exceptError(401, 'Refresh Token이 유효하지 않습니다'));
-
-      const newAccessToken = sign({data: {id}}, secretKey, {expiresIn: '1h'});
-      res.cookie('Authorization', newAccessToken);
-      req.cookies.Authorization = newAccessToken;
-      req.user = findUser;
-      next();
-    }
-    if (!isAccessExpired && isRefreshExpired) {
-      const newRefreshToken = sign({}, secretKey, {expiresIn: '1d'});
-      findUser.refresh = newRefreshToken;
-      findUser.save();
-
-      res.cookie('Refresh', newRefreshToken);
-      req.cookies.Refresh = newRefreshToken;
-      req.user = findUser;
-      next();
-    }
-    if (!isAccessExpired && !isRefreshExpired) {
-      req.user = findUser;
-      next();
-    }
+    if (typeof isAccessExpired === 'string')
+      throw new exceptError(401, `Error: ${isAccessExpired}`);
+    const {data} = isAccessExpired as JwtToken;
+    req.user = {id: data.id};
+    next();
   } catch (error) {
-    next(new exceptError(401, JSON.stringify(error)));
+    next(error);
   }
 };
